b358093e7c51565dbad0bb06b06b419aace40f6c
Jonatas <jonatastdb_jp@hotmail.com>
2019-03-13 18:25:04 -0300

    creation of components
    header, carousel, footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './container/main-page/main-page.component';
+import { HeaderComponent } from './components/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: MainPageComponent },
+  { path: 'header', component: HeaderComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainPageComponent } from './container/main-page/main-page.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainPageComponent,
+    FooterComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
