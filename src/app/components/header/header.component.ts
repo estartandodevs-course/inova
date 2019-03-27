470b211fc6429c8ef4e4651465c40a9efffd1453
Jonatas <jonatastdb_jp@hotmail.com>
2019-03-27 16:14:28 -0300

    modal-component
    create and config

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ModalComponent} from '../modal/modal.component';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor( private modalComponent: ModalComponent) {
+  }
 
   ngOnInit() {
   }
-
+  
+  openModal() {
+    this.modalComponent.openModal();
+  }
 }
