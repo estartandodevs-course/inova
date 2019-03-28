864d8f426ce196bc6760e29e0f534cf05de41146
Jonatas <jonatastdb_jp@hotmail.com>
2019-03-28 14:06:54 -0300

    Auto stash before merge of "develop" and "origin/develop"

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -11,32 +10,21 @@ export class ModalComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-      // setTimeout(()=>{
-      //   this.contentModal.description = "teste alteracao"
-      // }, 5000)
-  }
-  public contentModal = {
-    name: "ghj",
-    img: "",
-    description: "eu tu rlgsab ruo",
   }
 
-  public openModal(item){
-    console.log(this.contentModal.description);
-    this.contentModal.description = item.description;
+  @Input() contentModal
 
+  public openModal(){
     let modal = document.getElementById('myModal');
-    console.log(this.contentModal.description);
-
     if(modal.style.display == "none" || modal.style.display == ""){
       modal.style.display = "block";
     }else{
       modal.style.display = "none";
     };
   }
+
   public closeModal(){
     let modal = document.getElementById('myModal');
-    console.log(modal.style.display);
     if(modal.style.display == "block"){
       modal.style.display = "none";
     }
